607c165f966fb9dc6ddac99ba105edbd1792176b
Dominik Hezel <dominik.hezel@em.uni-frankfurt.de>
2023-07-05 16:55:22 +0200

    x

--- Welcome.py
+++ Welcome.py
@@ -1,18 +1,11 @@
 import streamlit as st
-from st_aggrid import AgGrid, GridUpdateMode
-from st_aggrid.grid_options_builder import GridOptionsBuilder
 import pandas as pd
 
+st.subheader('Data Science Short Course 2023 – Online Part')
 
-st.subheader('Welcome to the Data Science Short Course 2023')
+sel_language = st.radio('',('english', 'german'), horizontal=True)
 
-st.write('30 to 50 minutes of videos as preparation, exercises during online meeting')
-
-
-#---------------------------------#
-#------ Vorlesungen & Übungen ----#
-#---------------------------------#
-st.subheader('Wähle Deine Lerneinheit')
+st.divider()
 
 @st.cache_data
 def importCourseDatasheet():
@@ -21,38 +14,15 @@ def importCourseDatasheet():
 
 
 def useCourse(dfSearchAll):
-    import pandas as pd
-
-    tab1, tab2 = st.tabs(['Chapters', 'All'])
+    col1, col2 = st.columns((20,80))
+    with col1:
+        chapter_sel = st.radio('Select a Chapter', range(1,5), horizontal = True)
+    with col2:
+        dfSearchAll_tmp = dfSearchAll[dfSearchAll['Kapitel'] == chapter_sel]
+        topic_sel = st.selectbox('Select Unit', dfSearchAll_tmp['Titel'].tolist())
+        sel_row = dfSearchAll.loc[dfSearchAll[dfSearchAll['Titel'] == topic_sel].index[0]]
     
-    with tab1:
-        col1, col2 = st.columns((20,80))
-        with col1:
-            chapter_sel = st.radio('Select a Chapter', range(1,5), horizontal = True)
-        with col2:
-            dfSearchAll_tmp = dfSearchAll[dfSearchAll['Kapitel'] == chapter_sel]
-            topic_sel = st.selectbox('Select Unit', dfSearchAll_tmp['Titel'].tolist())
-            sel_row = dfSearchAll.loc[dfSearchAll[dfSearchAll['Titel'] == topic_sel].index[0]+1]
-
-    with tab2:
-        topic_sel_b = st.selectbox('Select a Topic', dfSearchAll['Titel'].tolist())
-        sel_row_b = dfSearchAll.loc[dfSearchAll[dfSearchAll['Titel'] == topic_sel_b].index[0]+1]
-
-
-    # dfSearchAll = dfSearchAll
-    # gd = GridOptionsBuilder.from_dataframe(dfSearchAll)
-    # gd.configure_pagination(enabled=True, paginationPageSize=5)
-    # gd.configure_default_column(editable=True,groupable=True)
-    # gd.configure_selection(selection_mode='single', use_checkbox=True)
-    # gridoptions = gd.build()
-    # grid_table = AgGrid(dfSearchAll, gridOptions=gridoptions, update_mode = GridUpdateMode.SELECTION_CHANGED, theme='material')
-    # sel_row = grid_table['selected_rows']
-    # st.write(sel_row)
-    # st.write(sel_row[0]['Youtube'])
-
-    
-    if len(sel_row) > 0:    
-        
+    if len(sel_row) > 0:
         col1, col2 = st.columns([3, 1])
         with col1:
             st.video(sel_row['Youtube'])
@@ -75,24 +45,14 @@ def useCourse(dfSearchAll):
                     st.write('keine vorhanden')
                 else:
                     st.write(vorlesung,uebungen,loesungen)
-                st.download_button(
-                    label="Lösung",
-                    data='jupyter_nb/'+sel_row['Lösungen ipynb'],
-                    file_name='Lösung '+topic_sel+'.ipynb'
-                )
+              
             with st.expander('Schlagworte', expanded=True):
                 if sel_row['Schlagworte'] != 'none':
                     st.write(sel_row['Schlagworte'])
                 else:
                     st.write('keine vorhannden')
-        
-        #st.subheader('Beschreibung')
-        st.write(sel_row['Beschreibung'])
 
-# =============================================================================
-#         else:
-#             st.subheader('Wähle eine Einheit aus obiger Liste')
-# =============================================================================
+        st.write(sel_row['Beschreibung'])
         
 
 dfSearchAll = importCourseDatasheet()
@@ -105,4 +65,3 @@ useCourse(dfSearchAll)
 
 st.sidebar.image('images/goethe-Logo.png', width=150)
 st.sidebar.write("Viele Wege führen zum Erfolg.")
-
